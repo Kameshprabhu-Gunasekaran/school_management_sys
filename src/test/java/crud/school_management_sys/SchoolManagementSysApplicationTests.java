7b9962f1de077326c8f4eef50d2e214a608cfb00
Kameshprabhu-Gunasekaran <kameshprabhu1998@gmail.com>
2025-01-02 12:01:18 -0800

    Added rest of the folders

---
+++ src/test/java/crud/school_management_sys/SchoolManagementSysApplicationTests.java
@@ -0,0 +1,13 @@
+package crud.school_management_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
